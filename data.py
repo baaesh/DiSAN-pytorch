512b1a55fdbfcbb8d26f2f8ea470fd2a9a84a6a0
Sanghwan Bae <sanghwan@europa.snu.ac.kr>
2018-07-31 16:16:57 +0900

    removed length limit

--- data.py
+++ data.py
@@ -13,18 +13,6 @@ class SNLI():
 
 		self.train, self.dev, self.test = datasets.SNLI.splits(self.TEXT, self.LABEL)
 
-		'''
-		for e in self.train.examples:
-			if len(e.premise) > 25 or len(e.hypothesis) > 25:
-				self.train.examples.remove(e)
-		for e in self.dev.examples:
-			if len(e.premise) > 25 or len(e.hypothesis) > 25:
-				self.dev.examples.remove(e)
-		for e in self.test.examples:
-			if len(e.premise) > 25 or len(e.hypothesis) > 25:
-				self.test.examples.remove(e)
-		'''
-
 		self.TEXT.build_vocab(self.train, self.dev, self.test, vectors=GloVe(name='6B', dim=300))
 		self.LABEL.build_vocab(self.train)
 
